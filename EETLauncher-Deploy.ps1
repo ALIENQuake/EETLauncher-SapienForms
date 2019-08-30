a7f3f23555c22a9a8d1637aebbbd20f5ad11a355
ALIENQuake <alienquake@hotmail.com>
2019-08-30 16:00:38 +0200

    Initial commit

---
+++ EETLauncher-Deploy.ps1
@@ -0,0 +1,4 @@
+Copy-Item "D:\Dokumenty\SAPIEN\PowerShell Studio\Projects\EETLauncher\EETLauncher.exe.config" "C:\Users\ALIEN\Documents\Visual Studio 2017\Projects\EETLauncher\Release"
+cd "C:\Users\ALIEN\Documents\Visual Studio 2017\Projects\EETLauncher\Release"
+git commit . -m "Add files via upload"
+git push
