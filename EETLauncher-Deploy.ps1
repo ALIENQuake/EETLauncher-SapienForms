46109688ea239dae46744a9b21810acba40a5e9b
ALIENQuake <alienquake@hotmail.com>
2024-11-01 12:44:25 +0100

    update

---
+++ EETLauncher-Deploy.ps1
@@ -0,0 +1,4 @@
+Copy-Item "D:\Projects\#EETLauncher\EETLauncher-SapienForms\EETLauncher.exe" "D:\Projects\#EETLauncher\EETLauncher"
+Set-Location "D:\Projects\#EETLauncher\EETLauncher"
+git commit . -m "Add files via upload"
+git push
